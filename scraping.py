8f06ce8a9186a70108ea451ff8edf179206cf510
Sheikh hamza <sheikhhamza012@gmail.com>
2019-11-30 18:25:58 +0500

    revised

--- scraping.py
+++ scraping.py
@@ -11,33 +11,38 @@ with open('out.csv', 'w') as f:
     i=0
     while i <= 200:
         i+=1
-        if(link in visited):
+        try:
+            if(link in visited):
+                i-=1
+                continue
+            r = requests.get(link) 
+            soup = BeautifulSoup(r.content, 'html5lib')
+            table = soup.find('article', attrs = {'class':'post'}) 
+            obj={'srNo':i,'link':link,'news':''}
+            visited.append(link)
+            link = soup.find('a',attrs={'rel':go})
+            if link == None :
+                if go == 'prev':
+                    break
+                i-=1
+                link=origin
+                go='prev'
+                continue
+            link= link['href']
+            label = soup.find('li',attrs={'class':'current-post-ancestor'})
+            if(label == None):
+                print ('label not detected ', i)
+                i-=1
+                print (i)
+                continue
+            obj['label']=label.find('a').string
+            for row in table.findAll('p'): 
+                if(row.string !=None):
+                    obj['news'] = obj['news'] + row.string
+            print(obj)
+            w.writerow(obj) 
+            print("////////////-------------")
+        except:
+            link = input('paste the random link from ary news, cant recurse further')
             i-=1
             continue
-        r = requests.get(link) 
-        soup = BeautifulSoup(r.content, 'html5lib')
-        table = soup.find('article', attrs = {'class':'post'}) 
-        obj={'srNo':i,'link':link,'news':''}
-        visited.append(link)
-        link = soup.find('a',attrs={'rel':go})
-        if link == None :
-            if go == 'prev':
-                break
-            i-=1
-            link=origin
-            go='prev'
-            continue
-        link= link['href']
-        label = soup.find('li',attrs={'class':'current-post-ancestor'})
-        if(label == None):
-            print ('label not detected ', i)
-            i-=1
-            print (i)
-            continue
-        obj['label']=label.find('a').string
-        for row in table.findAll('p'): 
-            if(row.string !=None):
-                obj['news'] = obj['news'] + row.string
-        print(obj)
-        w.writerow(obj) 
-        print("////////////-------------")
